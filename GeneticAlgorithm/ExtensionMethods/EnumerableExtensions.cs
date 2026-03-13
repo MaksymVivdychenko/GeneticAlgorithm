029de122246782f0836f52ec5b28d9b2d5947b90
vivdychenko.maxim2006@gmail.com <vivdychenko.maxim2006@gmail.com>
2026-03-13 12:17:38 +0200

    created extension method for Enumerable to find n max values

---
+++ GeneticAlgorithm/ExtensionMethods/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+namespace GeneticAlgorithm.ExtensionMethods;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TSource> TakeMax<TSource, TKey>(this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector, int n)
+    {
+        if (n <= 0)
+        {
+            return Enumerable.Empty<TSource>();
+        }
+
+        var pq = new PriorityQueue<TSource, TKey>();
+        foreach (var s in source)
+        {
+            TKey key = keySelector(s);
+            if (pq.Count < n)
+            {
+                pq.Enqueue(s, key);
+            }
+            else
+            {
+                pq.EnqueueDequeue(s, key);
+            }
+        }
+
+        return pq.UnorderedItems.Select(q => q.Element);
+    }
+}
