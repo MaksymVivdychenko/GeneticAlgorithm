bd42abba61e519be61885bc9f756502fdd27ecdf
Maksym Vivdychenko <vivdychenko.maxim2006@gmail.com>
2026-04-03 15:37:45 +0300

    added Sequential engine test

--- Testing.Console/Program.cs
+++ Testing.Console/Program.cs
@@ -29,20 +29,25 @@ public class Program
         var mutation = new PolygonMutation(width, height);
         var selection = new TournamentSelectionStrategy<List<ColoredPolygon>>();
         
-        var engineByThread = new ParallelGeneticAlgorithmEngine<List<ColoredPolygon>>(
-            fitness, crossover, mutation, selection, factory, Environment.ProcessorCount);
+        var engine = new ParallelGeneticEngine<List<ColoredPolygon>>(
+            fitness, crossover, mutation, selection, factory, populationSize, 2, 15, Environment.ProcessorCount);
+        var result = engine.Run(generations);
+
+        var fitnessValue = fitness.EvaluateFitness(result);
+        Console.WriteLine(fitnessValue);
+
         // var engineByThreadPool = new ParallelGeneticAlgorithmEngineByThreadPool<List<ColoredPolygon>>(
         //     fitness, crossover, mutation, selection, factory);
-        
-        string outputFolder = Path.Combine(Environment.CurrentDirectory, "EvolutionFrames");
 
-        // 6. Run the Engine!
-        Console.WriteLine($"Evolving {polygonCount} polygons over {generations} generations...");
-        var imageThread = engineByThread.Run(generations, populationSize, elitismCount: 2, mutationRate: 15);
-        //var imageThreadPool = engineByThread.Run(generations, populationSize, elitismCount: 2, mutationRate: 15);
-        ImageExporter.SaveChromosomeToDisk(imageThread, width, height, Path.Combine(outputFolder, "thread5000.png"));
-        //ImageExporter.SaveChromosomeToDisk(imageThread, width, height, Path.Combine(outputFolder, "threadPool5000.png"));
-        Console.WriteLine(Path.Combine(outputFolder, "thread5000.png"));
-        Console.WriteLine("Evolution Complete! Check the EvolutionFrames folder.");
+        // string outputFolder = Path.Combine(Environment.CurrentDirectory, "EvolutionFrames");
+        //
+        // // 6. Run the Engine!
+        // Console.WriteLine($"Evolving {polygonCount} polygons over {generations} generations...");
+        // var imageThread = engineByThread.Run(generations, populationSize, elitismCount: 2, mutationRate: 15);
+        // //var imageThreadPool = engineByThread.Run(generations, populationSize, elitismCount: 2, mutationRate: 15);
+        // ImageExporter.SaveChromosomeToDisk(imageThread, width, height, Path.Combine(outputFolder, "thread5000.png"));
+        // //ImageExporter.SaveChromosomeToDisk(imageThread, width, height, Path.Combine(outputFolder, "threadPool5000.png"));
+        // Console.WriteLine(Path.Combine(outputFolder, "thread5000.png"));
+        // Console.WriteLine("Evolution Complete! Check the EvolutionFrames folder.");
     }
 }
